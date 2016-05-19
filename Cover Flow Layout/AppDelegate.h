60d57aa176952faeeea4ba4783a63cc2b958ac00
moiseevanton <athidif94@gmail.com>
2016-05-19 09:02:50 -0400

    Initial Commit

---
+++ Cover Flow Layout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cover Flow Layout
+//
+//  Created by Anton Moiseev on 2016-05-19.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
